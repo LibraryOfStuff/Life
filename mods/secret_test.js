415cff490c7a968010e1340b1daaff87374e234f
slweeb <91897291+slweeb@users.noreply.github.com>
2026-06-19 00:20:38 -0400

    .

---
+++ mods/secret_test.js
@@ -0,0 +1,12 @@
+window.stateHistory = [];
+
+function logHistory() {
+    if (window.structuredClone === undefined) return;
+
+    stateHistory.push(structuredClone([currentPixels, currentRelations]));
+    if (stateHistory.length > 10) stateHistory.shift();
+}
+
+gameCanvas.addEventListener("mouseup", () => {
+    logHistory(".");
+})
